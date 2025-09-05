48cc3142e6a8f46a038c9b1d66d65d4f517afdbe
IT Sysdev Rco <it11.support@rcoid.com>
2025-09-05 09:59:31 +0800

    redirect non authenticated

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
 import { ofetch } from 'ofetch'
-
+import { router } from '../plugins/1.router/index'
 const baseURL = import.meta.env.VITE_API_BASE_URL || '/api'
 
 export const $api = ofetch.create({
@@ -13,7 +13,8 @@ export const $api = ofetch.create({
     if (response?.status === 401) {
       console.warn('Unauthorized, redirecting to login...')
       useCookie('accessToken').value = null // Remove token
-      useCookie('userData').value = null      
+      useCookie('userData').value = null
+      router.push('/login')      
     }
   },
 })
